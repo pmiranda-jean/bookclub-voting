c7ca528f55cb96f7449662ec100cb93e8598f5e3
Philippe Miranda-jean <philippemiranda-jean@Mac.ht.home>
2025-10-05 22:10:30 -0400

    mini change to test

--- config/settings.py
+++ config/settings.py
@@ -1,7 +1,7 @@
 # Application settings
 APP_TITLE = "Book Club Voting System"
 MAX_VOTES_PER_PERSON = 5
-TOTAL_POINTS = 100
+TOTAL_POINTS = 110
 TOP_BOOKS_TO_DISPLAY = 6
 
 # Goodreads scraping settings
@@ -10,4 +10,3 @@ REQUEST_TIMEOUT = 10
 # Data file paths
 BOOKS_DATA_PATH = 'data/books.json'
 VOTES_DATA_PATH = 'data/votes.json'
-
