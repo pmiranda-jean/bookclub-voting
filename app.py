0d139c36f2f582a6a058e3600d905500a1e45d1b
Philippe Miranda-jean <philippemiranda-jean@Mac.ht.home>
2025-11-23 10:50:14 -0500

    test

--- app.py
+++ app.py
@@ -377,7 +377,7 @@ elif page == "Results":
     voted_books = [b for b in books if b["total_points"] > 0]
     unvoted_books = [b for b in books if b["total_points"] == 0]
 
-    ranked_books = sorted(voted_books, key=lambda b: b["total_points"], reverse=False)
+    ranked_books = sorted(voted_books, key=lambda b: b["total_points"], reverse=True)
 
     st.header("📊 Results Overview")
 
